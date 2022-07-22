7619c10db60c9ae71b8521ccb65afdbbe35b40ed
Sylvia Makuch <sylvia@trimboli.io>
2022-07-22 16:39:21 -0600

    React Router added

--- employee-list/src/App.js
+++ employee-list/src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
-import NavBar from "./NavBar";
-import EmployeeList from "./EmployeeList";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import NavBar from "./NavBar.js";
+import EmployeeList from "./EmployeeList.js";
+import Home from "./Home.js";
+import Appointments from "./Appointments.js";
+import Clients from "./Clients.js";
 
 function App() {
   return (
- <>
- <NavBar />
-  <EmployeeList />
- </>
-    
+
+  <BrowserRouter>
+   <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/appointments" element={<Appointments />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/employeelist" element={<EmployeeList />} />
+      </Routes>
+    </BrowserRouter>
+ 
   );
 }
 
